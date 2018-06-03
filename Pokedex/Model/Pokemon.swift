e295fccbc7906266766f3051c0a768c81e96df09
Vishal Namala <Vishal@Vishals-MacBook-Air.local>
2018-06-03 19:04:55 -0400

    Day 1

---
+++ Pokedex/Model/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Vishal Namala on 6/3/18.
+//  Copyright © 2018 Vishal N. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex/View/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Vishal Namala on 6/3/18.
+//  Copyright © 2018 Vishal N. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    
+}
